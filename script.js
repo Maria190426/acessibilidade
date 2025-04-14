43ee758d1e03015c253586e41781d8ae07766923
Maria190426 <167318483+Maria190426@users.noreply.github.com>
2025-04-14 11:09:58 -0300

    .

--- script.js
+++ script.js
@@ -2,13 +2,19 @@ document.addEventListener('DOMContentLoaded', function(){
     const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
     const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
  
-    botaoDeAcessibilidade.addEventListener('click', function(){
+    botaoDeAcessibilidade.addEventListener('click', function (){
      botaoDeAcessibilidade.classList.toggle('rotacao-botao');
      opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
-    }
+ 
+     const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+     botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado)
+   
+    })
  
      const aumentaFonteBotao = document.getElementById('aumentar-fonte');
      const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+     
+     const alternaContraste = document.getElementById('alterna-contraste')
  
      let tamanhoAtualFonte = 1;
  
@@ -24,5 +30,14 @@ document.addEventListener('DOMContentLoaded', function(){
  
      })
  
-     
+     alternaContraste.addEventListener('click', function(){
+         document.body.classList.toggle('alto-contraste')
+     })
+ 
+ 
  })
+ 
+ ScrollReveal().reveal('#inicio', { delay: 500 });
+ ScrollReveal().reveal('#tropicalia', { delay: 500 });
+ ScrollReveal().reveal('#galeria', { delay: 500 });
+ ScrollReveal().reveal('#contato', { delay: 500 });
